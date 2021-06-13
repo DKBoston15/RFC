1f4ee26cd42bcb00c359e25e35eb32a4fcca3cf5
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-06-13 02:14:54 -0500

    Initial Workspace Creation Page

--- src/pages/WorkspaceSetup.js
+++ src/pages/WorkspaceSetup.js
@@ -1,9 +1,49 @@
 import React from "react"
+import { Box, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { useAuth } from "../config/auth"
+
+import WorkspaceForm from "../components/WorkspaceForm"
+
+const useStyles = makeStyles({
+    container: {
+        margin: "0 2em 0 2em"
+    },
+    grayText: {
+        color: "#969696"
+    },
+    textLeft: {
+        textAlign: "left",
+        lineHeight: ".2rem"
+    },
+    textRight: {
+        textAlign: "right"
+    }
+})
 
 export default function WorkspaceSetup() {
+    const classes = useStyles()
+    const { user } = useAuth()
+
     return (
-        <div>
-            <h1>Yo</h1>
-        </div>
+        <>
+            <Box display="flex" flex={12} className={classes.container}>
+                <Box flex={1} className={classes.textLeft}>
+                    <Typography
+                        variant="subtitle2"
+                        className={classes.grayText}
+                    >
+                        Logged in as:
+                    </Typography>
+                    <br />
+                    <Typography variant="subtitle2">{user.email}</Typography>
+                </Box>
+                <Box flex={10}></Box>
+                <Box flex={1} className={classes.textRight}>
+                    <Typography variant="subtitle2">Logout</Typography>
+                </Box>
+            </Box>
+            <WorkspaceForm />
+        </>
     )
 }
